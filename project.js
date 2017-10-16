51ddcf30fca28c2713140d43c17860d022c86d94
Ryan McCarthy <ryanxmccarthy@gmail.com>
2017-10-16 19:33:08 -0400

    added isotope rows

--- project.js
+++ project.js
@@ -29,36 +29,47 @@ $(document).ready(function() {
 
     var database = firebase.database();
 
+    var stateAbbrevs = ["AL", "AK", "AZ", "AR", 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"] 
+
+    var userStates = []
+
     $('#submit').on('click', function() {
         event.preventDefault();
 
-        var z = $('#search').val();
+        var z = $('#search').val().toUpperCase();
 
-        var key = 'O8BbfShMy4XlOSUGouj8GWAL2aYQpUavro0QLe12';
-        var queryURL = 'https://api.usa.gov/crime/fbi/ucr/estimates/states/' + z + '?page=3&per_page=10&output=json&api_key=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv';
+        if(stateAbbrevs.indexOf(z) === -1) {
+            $('#firstModal').modal('show')
+        } else {
+            if(userStates.indexOf(z) != -1) {
+                $('#secondModal').modal('show')
+            } else {
+                userStates.push(z)
+            
+                var key = 'O8BbfShMy4XlOSUGouj8GWAL2aYQpUavro0QLe12';
+                var queryURL = 'https://api.usa.gov/crime/fbi/ucr/estimates/states/' + z + '?page=3&per_page=10&output=json&api_key=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv';
 
-        $.ajax({
-                url: queryURL,
-                method: "GET"
-        })
-        .done(function(response) {
-            // console.log(response)
-            $("#crimes").append(`
-            <tr>    
-                <td>${response.results[0].state_abbr}</td>
-                <td>${response.results[0].population}</td>
-                <td>${response.results[0].violent_crime}</td>
-                <td>${response.results[0].homicide}</td>
-                <td>${response.results[0].rape_legacy}</td>
-                <td>${response.results[0].robbery}</td>
-                <td>${response.results[0].aggravated_assault}</td>
-            </tr>
-            `);
+                $.ajax({
+                        url: queryURL,
+                        method: "GET"
+                })
+                .done(function(response) {
+                    var newLi = $("<li class='table-like__item'><div class='name'>" + 
+                    response.results[0].state_abbr + "</div><div class='number'>" + 
+                    response.results[0].population + "</div>" + "<div class='number'>" + 
+                    response.results[0].violent_crime + "</div><div class='number'>" + 
+                    response.results[0].homicide + "</div>" + "<div class='number'>" + 
+                    response.results[0].rape_legacy + "</div><div class='number'>" + 
+                    response.results[0].robbery + "</div>" +
+                    // <div class='number'>" + response.results[0].aggravated_assault + "</div>
+                    "</li>"
+                    );
+                    $('.table-like').append(newLi).isotope( 'appended', newLi );
+                })
+            }
+        }     
 
-            var div = $("<div class='element-item'>")
-            div.innerHTML('<h4>' + response.results[0].state_abbr + '</h4>');
-            $('.grid').append(div)
-        })
+        $('#search').val('');
     })
 
     google.charts.load('current', {
@@ -95,33 +106,34 @@ $(document).ready(function() {
             var y = x.splice(3, 2);
             var z = y.join('');
 
-            var key = 'O8BbfShMy4XlOSUGouj8GWAL2aYQpUavro0QLe12';
-            var queryURL = 'https://api.usa.gov/crime/fbi/ucr/estimates/states/' + z + '?page=4&per_page=7&output=json&api_key=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv';
+            if (userStates.indexOf(z) != -1) {
+                $('#secondModal').modal('show')
+            } else {
+                userStates.push(z)
+            
+                var key = 'O8BbfShMy4XlOSUGouj8GWAL2aYQpUavro0QLe12';
+                var queryURL = 'https://api.usa.gov/crime/fbi/ucr/estimates/states/' + z + '?page=4&per_page=7&output=json&api_key=iiHnOKfno2Mgkt5AynpvPpUQTEyxE77jo1RU8PIv';
 
-            $.ajax({
-                    url: queryURL,
-                    method: "GET"
-            })
-            .done(function(response) {
-                // console.log(response)
-                $("#crimes").append(`
-                <tr>    
-                    <td>${response.results[0].state_abbr}</td>
-                    <td>${response.results[0].population}</td>
-                    <td>${response.results[0].violent_crime}</td>
-                    <td>${response.results[0].homicide}</td>
-                    <td>${response.results[0].rape_legacy}</td>
-                    <td>${response.results[0].robbery}</td>
-                    <td>${response.results[0].aggravated_assault}</td>
-                </tr>
-                `);
-
-                var newLi = $("<li class='table-like__item'><div class='name'>" + response.results[0].state_abbr + "</div><div class='crime'>" + response.results[0].violent_crime + "</div></li>");
-                $('.table-like').append(newLi)
-            })
+                $.ajax({
+                        url: queryURL,
+                        method: "GET"
+                })
+                .done(function(response) {
+                    var newLi = $("<li class='table-like__item'><div class='name'>" + 
+                    response.results[0].state_abbr + "</div><div class='number'>" + 
+                    response.results[0].population + "</div>" + "<div class='number'>" + 
+                    response.results[0].violent_crime + "</div><div class='number'>" + 
+                    response.results[0].homicide + "</div>" + "<div class='number'>" + 
+                    response.results[0].rape_legacy + "</div><div class='number'>" + 
+                    response.results[0].robbery + "</div>" +
+                    // <div class='number'>" + response.results[0].aggravated_assault + "</div>
+                    "</li>"
+                    );
+                    $('.table-like').append(newLi).isotope( 'appended', newLi );
+                })
+            }
         })
     };
-
 
     // isotope
         // external js: isotope.pkgd.js
@@ -155,5 +167,4 @@ $(document).ready(function() {
         $( this ).addClass('is-checked');
       });
     });
-
 })
